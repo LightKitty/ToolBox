fbba82e5928ff840bb7303efbaf0818b60b35908
wnmwi <383929371@qq.com>
2019-09-23 11:20:19 +0800

    修改TAes

--- ToolBox/TAes.cs
+++ ToolBox/TAes.cs
@@ -6,29 +6,48 @@ namespace ToolBox
 {
     public static class TAes
     {
+        #region 公开字段
         /// <summary>
-        /// Aes加解密钥必须32位
+        /// 默认密钥
         /// </summary>
-        //public const string defaultAesKey = "asekey";
-        public const CipherMode defaultCipherMode = CipherMode.ECB;
-        public const PaddingMode defaultPaddingMode = PaddingMode.PKCS7;
-        public const int defaultBitLength = 128;
-        public const string defultIv = null;
+        public const string defaultKey = "asekey";
 
         /// <summary>
-        /// 获取Aes密钥
+        /// 默认加密模式
+        /// </summary>
+        public const CipherMode defaultCipherMode = CipherMode.ECB;
+
+        /// <summary>
+        /// 默认填充类型
+        /// </summary>
+        public const PaddingMode defaultPaddingMode = PaddingMode.PKCS7;
+
+        /// <summary>
+        /// 默认密钥长度（位）
+        /// </summary>
+        public const int defaultKeySize = 128;
+
+        /// <summary>
+        /// 默认初始向量（ECB模式不需要）
+        /// </summary>
+        public const string defultIv = null;
+        #endregion
+
+        #region 公开方法
+        /// <summary>
+        /// 字符串转Aes密钥
         /// </summary>
         /// <param name="key">Aes密钥字符串</param>
-        /// <param name="bitLenght">密钥格式化位数</param>
+        /// <param name="keySize">密钥长度(128 192 256)</param>
         /// <returns>Aes密钥</returns>
-        private static byte[] GetAesKey(string key, int bitLenght)
+        public static byte[] GetAesKey(string key, int keySize)
         {
             if (string.IsNullOrEmpty(key))
             {
                 throw new ArgumentNullException("key", "Aes密钥不能为空");
             }
 
-            int byteLen = bitLenght / 8;
+            int byteLen = keySize / 8;
 
             byte[] temp = Encoding.UTF8.GetBytes(key);
             byte[] keyBytes = new byte[byteLen];
@@ -40,11 +59,11 @@ namespace ToolBox
         }
 
         /// <summary>
-        /// 获取初始向量
+        /// 字符串转初始向量
         /// </summary>
         /// <param name="ivStr">初始向量字符串</param>
         /// <returns>初始向量</returns>
-        private static byte[] GetIv(string ivStr)
+        public static byte[] GetIv(string ivStr)
         {
             byte[] iv = new byte[16];
             if (string.IsNullOrEmpty(ivStr)) return iv;
@@ -56,7 +75,9 @@ namespace ToolBox
 
             return iv;
         }
+        #endregion
 
+        #region 加密
         /// <summary>
         /// Aes加密
         /// </summary>
@@ -65,7 +86,7 @@ namespace ToolBox
         /// <returns>加密后的字符串</returns>
         public static string Encrypt(string source, string key)
         {
-            return Encrypt(source, key, defaultBitLength, defultIv, defaultCipherMode, defaultPaddingMode);
+            return Encrypt(source, key, defaultKeySize, defultIv, defaultCipherMode, defaultPaddingMode);
         }
 
         /// <summary>
@@ -73,31 +94,50 @@ namespace ToolBox
         /// </summary>
         /// <param name="source">源字符串</param>
         /// <param name="key">aes密钥</param>
-        /// <param name="bitLength">密钥格式化位数</param>
-        /// <param name="ivString">初始化向量字符串</param>
+        /// <param name="keySize">密钥位数</param>
+        /// <param name="iv">初始化向量字符串</param>
         /// <param name="cipherMode">加密模式</param>
         /// <param name="paddingMode">加密填充类型</param>
         /// <returns>加密后字符串</returns>
-        public static string Encrypt(string source, string key, int bitLength, string ivString, CipherMode cipherMode, PaddingMode paddingMode)
+        public static string Encrypt(string source, string key, int keySize, string iv, CipherMode cipherMode, PaddingMode paddingMode)
+        {
+            byte[] sourceBytes= Encoding.UTF8.GetBytes(source);
+            byte[] keyBytes = GetAesKey(key, keySize);
+            byte[] ivBytes = GetIv(iv);
+            byte[] resultBytes = Encrypt(sourceBytes, keyBytes, ivBytes, cipherMode, paddingMode);
+            return Convert.ToBase64String(resultBytes, 0, resultBytes.Length);
+        }
+
+        /// <summary>
+        /// AES加密
+        /// </summary>
+        /// <param name="source">源数据</param>
+        /// <param name="key">密钥</param>
+        /// <param name="iv">初始向量（16字节）</param>
+        /// <param name="cipherMode">加密模式</param>
+        /// <param name="paddingMode">加密填充类型</param>
+        /// <returns>加密后数据</returns>
+        public static byte[] Encrypt(byte[] source, byte[] key, byte[] iv, CipherMode cipherMode, PaddingMode paddingMode)
         {
             using (AesCryptoServiceProvider aesProvider = new AesCryptoServiceProvider())
             {
-                aesProvider.KeySize = bitLength;
-                aesProvider.Key = GetAesKey(key, bitLength);
-                aesProvider.IV = GetIv(ivString);
+                aesProvider.KeySize = key.Length * 8;
+                aesProvider.Key = key;
+                aesProvider.IV = iv;
                 aesProvider.Mode = cipherMode;
                 aesProvider.Padding = paddingMode;
                 using (ICryptoTransform cryptoTransform = aesProvider.CreateEncryptor())
                 {
-                    byte[] inputBuffers = Encoding.UTF8.GetBytes(source);
-                    byte[] results = cryptoTransform.TransformFinalBlock(inputBuffers, 0, inputBuffers.Length);
+                    byte[] result = cryptoTransform.TransformFinalBlock(source, 0, source.Length);
                     aesProvider.Clear();
                     aesProvider.Dispose();
-                    return Convert.ToBase64String(results, 0, results.Length);
+                    return result;
                 }
             }
         }
+        #endregion
 
+        #region 解密
         /// <summary>
         /// Aes解密
         /// </summary>
@@ -106,7 +146,7 @@ namespace ToolBox
         /// <returns>解密后的字符串</returns>
         public static string Decrypt(string source, string key)
         {
-            return Decrypt(source, key, defaultBitLength, defultIv, defaultCipherMode, defaultPaddingMode);
+            return Decrypt(source, key, defaultKeySize, defultIv, defaultCipherMode, defaultPaddingMode);
         }
 
         /// <summary>
@@ -114,28 +154,47 @@ namespace ToolBox
         /// </summary>
         /// <param name="source">源字符串</param>
         /// <param name="key">aes密钥</param>
-        /// <param name="bitLength">密钥格式化位数</param>
-        /// <param name="ivString">初始化向量字符串</param>
+        /// <param name="keySize">密钥格式化位数</param>
+        /// <param name="iv">初始化向量字符串</param>
         /// <param name="cipherMode">加密模式</param>
         /// <param name="paddingMode">加密填充类型</param>
         /// <returns>解密后的字符串</returns>
-        public static string Decrypt(string source, string key, int bitLength, string ivString, CipherMode cipherMode, PaddingMode paddingMode)
+        public static string Decrypt(string source, string key, int keySize, string iv, CipherMode cipherMode, PaddingMode paddingMode)
+        {
+            byte[] sourceBytes = Convert.FromBase64String(source);
+            byte[] keyBytes= GetAesKey(key, keySize);
+            byte[] ivBytes= GetIv(iv);
+            byte[] resultBytes = Decrypt(sourceBytes, keyBytes, ivBytes, cipherMode, paddingMode);
+            return Encoding.UTF8.GetString(resultBytes);
+        }
+
+        /// <summary>
+        /// AES解密
+        /// </summary>
+        /// <param name="source">源数据</param>
+        /// <param name="key">密钥</param>
+        /// <param name="iv">初始化向量（16字节）</param>
+        /// <param name="cipherMode">加密模式</param>
+        /// <param name="paddingMode">加密填充类型</param>
+        /// <returns>解密后数据</returns>
+        public static byte[] Decrypt(byte[] source, byte[] key, byte[] iv, CipherMode cipherMode, PaddingMode paddingMode)
         {
             using (AesCryptoServiceProvider aesProvider = new AesCryptoServiceProvider())
             {
-                aesProvider.KeySize = bitLength;
-                aesProvider.Key = GetAesKey(key, bitLength);
-                aesProvider.IV = GetIv(ivString);
+                aesProvider.KeySize = key.Length * 8;
+                aesProvider.Key = key;
+                aesProvider.IV = iv;
                 aesProvider.Mode = cipherMode;
                 aesProvider.Padding = paddingMode;
                 using (ICryptoTransform cryptoTransform = aesProvider.CreateDecryptor())
                 {
-                    byte[] inputBuffers = Convert.FromBase64String(source);
-                    byte[] results = cryptoTransform.TransformFinalBlock(inputBuffers, 0, inputBuffers.Length);
+                    byte[] result = cryptoTransform.TransformFinalBlock(source, 0, source.Length);
                     aesProvider.Clear();
-                    return Encoding.UTF8.GetString(results);
+                    aesProvider.Dispose();
+                    return result;
                 }
             }
         }
+        #endregion
     }
 }
