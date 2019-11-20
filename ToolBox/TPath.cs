73544e6b5e5e87d3a98888c96a7275441fbfac2a
wnmwi <383929371@qq.com>
2019-11-20 15:07:32 +0800

    add TTime

--- ToolBox/TPath.cs
+++ ToolBox/TPath.cs
@@ -16,34 +16,41 @@ namespace ToolBox
         /// 获取路径下所有文件以及子文件夹中文件  
         /// </summary>
         /// <param name="path">绝对路径</param>
+        /// <param name="recursive">是否包含子目录</param> 
         /// <returns></returns>
-        public static List<string> GetFilePaths(string path)
+        public static List<string> GetFilePaths(string path, bool recursive = false)
         {
-            return GetFilePaths(path, new List<string>());
+            return GetFilePaths(path, new List<string>(), recursive);
         }
 
         /// <summary>  
         /// 获取路径下所有文件以及子文件夹中文件  
         /// </summary>  
         /// <param name="path">全路径根目录</param>  
-        /// <param name="fileList">存放所有文件的全路径</param>  
+        /// <param name="fileList">存放所有文件的全路径</param> 
+        /// <param name="recursive">是否包含子目录</param> 
         /// <returns></returns>  
-        private static List<string> GetFilePaths(string path, List<string> fileList)
+        private static List<string> GetFilePaths(string path, List<string> fileList, bool recursive = false)
         {
             DirectoryInfo dir = new DirectoryInfo(path);
             FileInfo[] fil = dir.GetFiles();
-            DirectoryInfo[] dii = dir.GetDirectories();
             foreach (FileInfo f in fil)
             {
                 //int size = Convert.ToInt32(f.Length);  
                 //long size = f.Length;
                 fileList.Add(f.FullName);//添加文件路径到列表中  
             }
-            //获取子文件夹内的文件列表，递归遍历  
-            foreach (DirectoryInfo d in dii)
+
+            if(recursive)
             {
-                GetFilePaths(d.FullName, fileList);
+                DirectoryInfo[] dii = dir.GetDirectories();
+                //获取子文件夹内的文件列表，递归遍历  
+                foreach (DirectoryInfo d in dii)
+                {
+                    GetFilePaths(d.FullName, fileList);
+                }
             }
+
             return fileList;
         }
     }
---
+++ ToolBox/TTime.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ToolBox
+{
+    public static class TTime
+    {
+        private static readonly DateTime startTime = TimeZone.CurrentTimeZone.ToLocalTime(new DateTime(1970, 1, 1)); // 当地时区起始计算时间
+
+        /// <summary>
+        /// 获取当前JavaScript时间戳
+        /// </summary>
+        /// <returns></returns>
+        public static long GetJsTimestampNow()
+        {
+            return GetJsTimestamp(DateTime.Now);
+        }
+
+        /// <summary>
+        /// 获取JavaScript时间戳
+        /// </summary>
+        /// <returns></returns>
+        public static long GetJsTimestamp(DateTime time)
+        {
+            long timeStamp = (long)(time - startTime).TotalMilliseconds; // 相差毫秒数
+            return timeStamp;
+        }
+
+        /// <summary>
+        /// js时间戳转DateTime
+        /// </summary>
+        /// <param name="jsTimestamp"></param>
+        /// <returns></returns>
+        public static DateTime JsTimestampToDatetime(long jsTimestamp)
+        {
+            return startTime.AddMilliseconds(jsTimestamp);
+        }
+
+        /// <summary>
+        /// 获取当前时间Unix时间戳
+        /// </summary>
+        /// <returns></returns>
+        public static long GetUnixTimestampNow()
+        {
+            return GetUnixTimestamp(DateTime.Now);
+        }
+
+        /// <summary>
+        /// 获取Unix时间戳
+        /// </summary>
+        /// <param name="time"></param>
+        /// <returns></returns>
+        public static long GetUnixTimestamp(DateTime time)
+        {
+            long timeStamp = (long)(time - startTime).TotalSeconds; // 相差毫秒数
+            return timeStamp;
+        }
+
+        /// <summary>
+        /// unix时间戳转DateTime
+        /// </summary>
+        /// <param name="unixTimestamp"></param>
+        /// <returns></returns>
+        public static DateTime UnixTimestampToDatetime(long unixTimestamp)
+        {
+            return startTime.AddSeconds(unixTimestamp);
+        }
+    }
+}
